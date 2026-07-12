00813a9926a695e5c72ccefa19ea6ba1775dfea1
muhsin <muhsintp.develop@gmail.com>
2026-07-12 07:07:13 +0530

    update proxy target ports in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -93,12 +93,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'http://127.0.0.1:7000',
         changeOrigin: true,
         secure: false,
       },
       '/socket.io': {
-        target: 'http://127.0.0.1:5000',
+        target: 'http://127.0.0.1:7000',
         changeOrigin: true,
         ws: true,
       },
