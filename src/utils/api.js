a7feeaac18a74696b97feabe4145fa71f657623b
muhsin <muhsintp.develop@gmail.com>
2026-08-19 19:52:44 +0530

    chore: update baseURL in API configuration to use default path

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://82.29.165.57:7000/api',
+    baseURL: import.meta.env.VITE_API_URL || '/api',
     withCredentials: true, // Send cookies if any
     headers: {
         'Content-Type': 'application/json',
